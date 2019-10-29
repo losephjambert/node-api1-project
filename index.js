8a8c25ff71ce3ae4aa894ea69f1e9bd0f7f11e57
Joe <josephc.lambert@gmail.com>
2019-10-29 15:19:52 -0700

    PUT req for updating user by ID

--- index.js
+++ index.js
@@ -83,5 +83,24 @@ server.post('/api/users', (request, response) => {
     });
 });
 
+// update user
+server.put('/api/users/:id', (request, response) => {
+  const { name, bio } = request.body;
+  const { id } = request.params;
+  if (!name || !bio) {
+    response.status(400).send({
+      errorMessage: 'Please provide name and bio for the user.',
+    });
+  }
+  db.update(id, { name, bio })
+    .then(updateResponse => {
+      updateResponse === 0 && response.send(404);
+      response.sendStatus(200);
+    })
+    .catch(error => {
+      response.sendStatus(500, error);
+    });
+});
+
 const port = 5000;
 server.listen(port, () => console.log(`Listening on port ${port}`));
